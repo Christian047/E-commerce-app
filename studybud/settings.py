6c3c8392c24c2cc4f930826dcea2ef62d8d6d600
Christian047 <126990667+Christian047@users.noreply.github.com>
2024-03-22 11:43:31 -0800

    Update settings3.py

--- studybud/settings.py
+++ studybud/settings.py
@@ -142,7 +142,7 @@ STATICFILES_DIRS = [
     BASE_DIR /'static']
 
 # tHIS IS THE STORAGE BASE FOR USER UPLOADED IMAGES
-# STATIC_ROOT =
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
