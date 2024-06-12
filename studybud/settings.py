14e33ef03392fd0bb596a9c9074217505eb2d227
Christian047 <chrispadiga@gmail.com>
2024-06-12 13:04:13 +0100

    procfile

--- studybud/settings.py
+++ studybud/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-import django-heroku
+import django_heroku
 import dj_database_url
 from decouple import config
 
