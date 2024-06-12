98d4a02ac0bd74150ff16e99473a122ae9f64cc0
Christian047 <chrispadiga@gmail.com>
2024-06-12 13:29:39 +0100

    django

--- studybud/settings.py
+++ studybud/settings.py
@@ -166,4 +166,4 @@ PAYSTACK_SECRET_KEY = os.environ.get('PAYSTACK_SECRET_KEY')
 PAYSTACK_PUBLIC_KEY = os.environ.get('PAYSTACK_PUBLIC_KEY')
 
 
--django.heroku.settings(locals())
+django_heroku.settings(locals())
